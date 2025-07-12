f5f2bba4218b6f70a707c3ecd4dacb92da9d9c4e
meetgoti07 <meetgoti07@gmail.com>
2025-07-12 09:47:08 +0530

    Auth setup

---
+++ lib/auth.ts
@@ -0,0 +1,54 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+import {PrismaClient} from "@/lib/generated/prisma";
+import process from "node:process";
+import {nextCookies} from "better-auth/next-js";
+import {admin, emailOTP} from "better-auth/plugins";
+
+const prisma = new PrismaClient();
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+    appName: "stackit",
+    debug: true,
+    emailAndPassword: {
+        enabled: true
+    },
+    socialProviders: {
+        google: {
+           clientId: process.env.GOOGLE_CLIENT_ID as string,
+           clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+        microsoft: {
+           clientId: process.env.MICROSOFT_CLIENT_ID as string,
+           clientSecret: process.env.MICROSOFT_CLIENT_SECRET as string,
+        },
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        }
+    },
+    plugins: [
+            nextCookies(),
+            admin(),
+            emailOTP({
+            async sendVerificationOTP({ email, otp}) {
+                await sendOTPEmail(email, otp);
+            },
+            otpLength: 6,
+            expiresIn: 300, // 5 minutes
+            allowedAttempts: 3,
+        })
+    ],
+    session: {
+        cookieCache: {
+            enabled: true,
+            maxAge: 5 * 60 // Cache duration in seconds
+        },
+        expiresIn: 60 * 60 * 24 * 2, // 7 days
+        updateAge: 60 * 60 * 24 // 1 day (every 1 day the session expiration is updated)
+    }
+});
